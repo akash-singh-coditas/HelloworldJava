da363f2bd2b579d4d0f2c2a7129d087918640c17
Akash Singh <akash.singh@coditas.com>
2022-03-21 13:58:35 +0530

    added changes

--- file.java
+++ file.java
@@ -3,7 +3,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         for(int i = 0;i<10;i++){
-		System.out.println("running using git commit");
+		System.out.println("running using git push");
          } 
     }
 }
